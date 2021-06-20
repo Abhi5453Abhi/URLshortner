751df76598d6f9346e23d41ed20fb40a3b9e05f5
Abhishek Arora <abhishek@192.168.1.2>
2021-06-21 01:00:03 +0530

    Version 1.0 ready

--- index.js
+++ index.js
@@ -6,26 +6,30 @@ const app = express();
 app.set('view engine' ,'ejs');
 app.use(express.urlencoded({extended:false}))
 
-mongoose.connect("mongodb+srv://Mango_OP:Jasveen@2020@cluster0.n48fr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
-    useNewUrlParser:true, useUnifiedTopology:true
-});
+// mongoose.connect("mongodb+srv://Mango_OP:Jasveen@2020@cluster0.n48fr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+//     useNewUrlParser:true, useUnifiedTopology:true
+// });
 app.listen(process.env.PORT || 3000);
 
 app.get('/',async (req,res) => {
-    const shortURLs = await shortURL.find();
-    res.render('index',{shortURLs:shortURLs})
+    res.end("Started");
 })
-app.post('/shortURLs',async (req,res) => {
-    await shortURL.create({
-        full:req.body.fullURL
-    })
-    res.redirect('/')
-});
 
-app.get('/:shortUrl',async (req,res) => {
-    const shortUrl = await shortURL.findOne({short: req.params.shortUrl})
-    console.log(shortUrl.full);
-    if(shortUrl === null || shortUrl === undefined) res.end("Sorry couldn't find the URL");
+// app.get('/',async (req,res) => {
+//     const shortURLs = await shortURL.find();
+//     res.render('index',{shortURLs:shortURLs})
+// })
+// app.post('/shortURLs',async (req,res) => {
+//     await shortURL.create({
+//         full:req.body.fullURL
+//     })
+//     res.redirect('/')
+// });
 
-    res.redirect(shortUrl.full);
-})
+// app.get('/:shortUrl',async (req,res) => {
+//     const shortUrl = await shortURL.findOne({short: req.params.shortUrl})
+//     console.log(shortUrl.full);
+//     if(shortUrl === null || shortUrl === undefined) res.end("Sorry couldn't find the URL");
+
+//     res.redirect(shortUrl.full);
+// })
